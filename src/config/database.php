696852dcfaabdcfd1075f5453c563fb7d31d0d58
James Kim <73400219+shallowsmith@users.noreply.github.com>
2023-11-22 01:00:12 -0800

    Add database configuration and connection code

---
+++ src/config/database.php
@@ -0,0 +1,16 @@
+// Database configuration
+$host = 'localhost';
+$username = 'root'; 
+$password = ''; 
+$database = 'test1';
+
+// Create a connection
+$connection = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+// Connection successful
+echo "Connected to the database successfully!";
